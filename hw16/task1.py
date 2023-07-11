972b16924c5c408cbbad36329b8e0b1be4117f29
olex <olexandr.samus.2004@gmail.com>
2023-07-11 20:57:58 +0300

    add hw17

--- hw16/task1.py
+++ hw16/task1.py
@@ -9,5 +9,14 @@
 # необходимо, чтобы старая информация в файле тоже сохранялась, поэтому при работе с
 # файлом, откройте файл в режиме "a", добавьте сотрудника record в файл и закройте его
 # мы пока не используем менеджер контекста with
+from math import *
+print(factorial(5), end=' ')
+print(sqrt(16))
+# infile = open(file = "c:\\test.txt", mode="r")
+def add_employee_to_file(record: str, path: str):
+    file = open(path, 'a', encoding='utf-8')
+    file.write(record + '\n')
+    file.close()
 
-def add_employee_to_file(record, path):
+
+add_employee_to_file("Drake Mikelsson, 19", 'test.txt')
