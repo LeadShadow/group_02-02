b0d4bd4bd257be6b9166ed4527dccc676c7bfa9d
olex <olexandr.samus.2004@gmail.com>
2023-08-02 18:49:37 +0300

    add lesson21

--- lesson20_21/main.py
+++ lesson20_21/main.py
@@ -61,3 +61,64 @@
 # распакованное содержимое архива переносится в папку archives в подпапку, названную точно так же,
 # как и архив, но без расширения в конце;
 # файлы, расширения которых неизвестны, остаются без изменений.
+from pathlib import Path
+import shutil
+import sys
+from normalize import normalize
+import file_parser as parser
+
+# filename -> C:\Users\pc\Desktop\заняття\group_02-02\hw5\мій_ткт_файл.txt
+# filename.name -> miy_tkt_fail
+def handler_files(filename: Path, target_folder: Path):
+    # Створюємо папку
+    target_folder.mkdir(exist_ok=True, parents=True)
+    # Замінюємо шлях до файлу + перетворюємо кирилицю на латиницю
+    filename.replace(target_folder / normalize(filename.name))
+
+def handler_archive(filename: Path, target_folder: Path):
+    # Створюємо папку для архівів
+    target_folder.mkdir(exist_ok=True, parents=True)
+    # Створюємо папку і розпакуємо туди архів
+    # Беремо суфікс у файла і забираємо replace(filename.suffix, '')
+    folder_for_file = target_folder / normalize(filename.name.replace(filename.suffix, ''))
+    try:
+        shutil.unpack_archive(str(filename.resolve()), str(folder_for_file.resolve()))
+    except shutil.ReadError:
+        print(f'Це не архів! {filename}')
+        folder_for_file.rmdir()
+        return None
+    filename.unlink()
+
+
+
+def handler_folders(folder: Path):
+    try:
+        folder.rmdir()
+    except OSError:
+        print(f'Не вдалось видалити папку {folder.name}')
+
+
+
+def main(folder: Path):
+    parser.scan(folder)
+
+    for file in parser.JPEG_IMAGES:
+        handler_files(file, folder / 'images' / 'JPEG')
+    for file in parser.JPG_IMAGES:
+        handler_files(file, folder / 'images' / 'JPG')
+    for file in parser.SVG_IMAGES:
+        handler_files(file, folder / 'images' / 'SVG')
+    for file in parser.PNG_IMAGES:
+        handler_files(file, folder / 'images' / 'PNG')
+
+
+    for file in parser.DOCX_DOCUMENTS:
+        handler_files(file, folder / 'documents' / 'DOCX')
+    for file in parser.TXT_DOCUMENTS:
+        handler_files(file, folder / 'documents' / 'TXT')
+
+
+if __name__ == "__main__":
+    folder_for_scan = Path(input('Enter path to folder which you want clean: '))
+    print(f'Start in folder {folder_for_scan.resolve()}')
+    main(folder_for_scan.resolve())
